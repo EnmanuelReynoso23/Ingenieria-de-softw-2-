45743aaa77e9d3d15f71caa11ee9cd4561a1b0ab
Ryan Durán Rey <100086388@p.uapa.edu.do>
2026-07-06 17:34:31 -0400

    Creación de componente de conexión con BD

---
+++ DB/conexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using Npgsql;
+
+namespace InventoryApp.DB
+{
+    public class ConexionDB
+    {
+        private string conexionString;
+
+        public ConexionDB()
+        {
+            conexionString = "Host=db.pztlyjvnukuikgxnkgxo.supabase.co;" +
+                "Username=postgres;" +
+                "Password=o25rx0LEoO57IhDm;" +
+                "Database=postgres;" +
+                "Port=5432;" +
+                "Pooling=true;" +
+                "SSL Mode=Require;" +
+                "Trust Server Certificate=true";
+        }
+
+        public NpgsqlConnection GetConnection()
+        {
+            try
+            {
+                NpgsqlConnection conn = new NpgsqlConnection(conexionString);
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se pudo conectar con la base de datos:" + ex.Message);
+            }
+        }
+    }
+}
